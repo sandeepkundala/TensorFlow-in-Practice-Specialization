f97ccfd8b06627c21457ec7744d1d8a3acfea733
Sandeep Kundala <sandeepkundala@gmail.com>
2019-08-04 16:32:50 -0700

    Update and rename EX2-Fashion-MNIST.py to EX2-Handwriting-Recognition.py

--- Intro-to-TF/EX2-Handwriting-Recognition.py
+++ Intro-to-TF/EX2-Handwriting-Recognition.py
@@ -15,22 +15,16 @@ I've started the code for you below -- how would you finish it?
 import tensorflow as tf
 mnist = tf.keras.datasets.mnist
 
-# YOUR CODE SHOULD START HERE
 class myCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs={}):
     if(logs.get('acc')>0.99):
       print("\nReached 60% accuracy so cancelling training!")
       self.model.stop_training = True
-# YOUR CODE SHOULD END HERE
-
-
 
 callbacks = myCallback()
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
-# YOUR CODE SHOULD START HERE
 x_train = x_train/255.0
 x_test = x_test/255.0
-# YOUR CODE SHOULD END HERE
 model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
                                     tf.keras.layers.Dense(1024, activation=tf.nn.relu), 
                                     tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
@@ -39,6 +33,5 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# YOUR CODE SHOULD START HERE
 model.fit(x_train,y_train,epochs=10, callbacks=[callbacks])
-# YOUR CODE SHOULD END HERE
+
